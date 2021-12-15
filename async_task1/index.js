bf13eccb2c2a936f79ab831ba9488e1b01bccf30
Matvey Prupas <matvey_prupas@epam.com>
2021-12-15 09:49:41 +0200

    fix: remove comments

--- async_task1/index.js
+++ async_task1/index.js
@@ -1,14 +1,3 @@
-// let promise = new Promise((resolve) => {
-//         console.log(1);
-//     resolve();
-// }).then(()	=>	{
-//     console.log(2);
-// }).then(()	=>	{
-//     console.log(3);
-// })
-// console.log(4);
-//1, 4, 2, 3
-
 class MyPromise extends Promise {
     constructor (resolve, reject) {
         super(resolve, reject);
@@ -29,4 +18,5 @@ let myPromise = new MyPromise((resolve) => {
     console.log(3);
 })
 console.log(4);
+
 // 1,	2,	4,	3
