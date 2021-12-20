298e57cdd6c719d37b5373ae031e4b6ee56732c5
Matvey Prupas <matvey_prupas@epam.com>
2021-12-20 22:00:06 +0200

    fix: problems with bottom and right borders

--- events_task2/index.js
+++ events_task2/index.js
@@ -22,29 +22,43 @@ tdArr.forEach((el) => {
     let left = el.offsetLeft;
     let top = el.offsetTop;
     el.setAttribute("style", `left: ${left}px; top: ${top}px`);
-    // el.classList.add("td");
-});
-
-tdArr.forEach((el) => {
-    // el.classList.add("td");
+    setTimeout(() => el.classList.add("td"), 0);
 });
 
 tableContainer.addEventListener('mousedown', (e) => {
     let target = e.target;
 
     if(target.tagName === 'TD') {
-        let clickX = e.clientX - e.target.offsetLeft;
-        let clickY = e.clientY - e.target.offsetTop;
+        // let startX = e.clientX;
+        // let startY = e.clientY;
+        let clickX = e.clientX - target.offsetLeft;
+        let clickY = e.clientY - target.offsetTop;
         
-        console.log(clickX);
-        console.log(clickY);
+        // console.log(clickX);
+        // console.log(clickY);
         target.classList.add("td");
 
 
         target.addEventListener("mousemove", (e) => {
+            if (target) {
+                // console.log(e.clientX - startX + clickX);
+                // let left = (e.clientX - startX + clickX >= cellDimention - 2) ? e.clientX - clickX : e.clientX - clickX; 
+                // console.log(e.clientX - startX + clickX >= cellDimention - 2, left);
 
+                target.setAttribute("style", `left: ${e.clientX - clickX}px; top: ${e.clientY - clickY}px`);
+                console.log(e.type);
+            }
         });
 
+        target.addEventListener("mouseout", (e) => {
+            target = null;
+            return;
+        });
+
+
+        target.addEventListener("mouseup", (e) => {
+            target = null;
+            return;
+        });
     }
 });
-
