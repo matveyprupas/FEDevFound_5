c7f5c1dec387cc76ec978514bdc96b4becd7795c
Matvey Prupas <matvey_prupas@epam.com>
2021-12-21 11:11:10 +0200

    fix: fix problems all is good

--- events_task2/index.js
+++ events_task2/index.js
@@ -11,7 +11,6 @@ for (let i = 1; i <= tableQuantityCell; i++) {
         tableColumn.setAttribute("width", cellDimention+'px');
         tableColumn.setAttribute("height", cellDimention+'px');
         tableString.append(tableColumn);
-        // console.log(tableColumn.offsetLeft)
     }
     tableContainer.append(tableString);
 }
@@ -21,44 +20,40 @@ let tdArr = document.querySelectorAll('td');
 tdArr.forEach((el) => {
     let left = el.offsetLeft;
     let top = el.offsetTop;
-    el.setAttribute("style", `left: ${left}px; top: ${top}px`);
+
+    el.style.left = left + 'px';
+    el.style.top = top + 'px';
+    el.style.zIndex = 0;
+    
     setTimeout(() => el.classList.add("td"), 0);
 });
 
+let target = null;
+
 tableContainer.addEventListener('mousedown', (e) => {
-    let target = e.target;
+    target = e.target;
 
     if(target.tagName === 'TD') {
-        // let startX = e.clientX;
-        // let startY = e.clientY;
         let clickX = e.clientX - target.offsetLeft;
         let clickY = e.clientY - target.offsetTop;
-        
-        // console.log(clickX);
-        // console.log(clickY);
-        target.classList.add("td");
 
+        target.style.zIndex++;
+        target.classList.add("td");
 
         target.addEventListener("mousemove", (e) => {
             if (target) {
-                // console.log(e.clientX - startX + clickX);
-                // let left = (e.clientX - startX + clickX >= cellDimention - 2) ? e.clientX - clickX : e.clientX - clickX; 
-                // console.log(e.clientX - startX + clickX >= cellDimention - 2, left);
-
-                target.setAttribute("style", `left: ${e.clientX - clickX}px; top: ${e.clientY - clickY}px`);
-                console.log(e.type);
+                target.style.left = e.clientX - clickX + 'px';
+                target.style.top = e.clientY - clickY + 'px';
             }
-        });
-
-        target.addEventListener("mouseout", (e) => {
-            target = null;
-            return;
-        });
-
-
-        target.addEventListener("mouseup", (e) => {
-            target = null;
-            return;
         });
     }
 });
+
+tableContainer.addEventListener("mouseout", endOfMove);
+tableContainer.addEventListener("mouseup", endOfMove);
+
+function endOfMove () {
+    target.style.zIndex--;
+    target = null;
+    return;
+}
