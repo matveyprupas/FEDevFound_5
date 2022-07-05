2ab864578a3124931f41f493af6d1e627275a389
Matvey Prupas <matvey_prupas@epam.com>
2022-07-05 22:09:32 +0300

    feat: implement Inventory

--- 02_oop_part_1/code/src/Inventory.ts
+++ 02_oop_part_1/code/src/Inventory.ts
@@ -1,1 +1,36 @@
-// your code goes here
+import { Item } from "./Item";
+
+export class Inventory {
+    items: Item[];
+
+    constructor (items?: Item[]) {
+        this.items = !!items ? items : [];
+    }
+
+    addItem(item: Item): void {
+        this.items.push(item);
+    }
+
+    sort(comparator?: number): Item[] {
+        
+        if (!!comparator) {
+            return this.items.sort(( a, b ) => a.weight - b.weight )
+        } else {
+            return this.items.sort((a, b) => {
+                const nameA = a.value.toUpperCase();
+                const nameB = b.value.toUpperCase();
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+                return 0;
+              });
+        }
+    }
+
+    toString(): string {
+        return this.items.join(', ');
+    }
+}
--- 02_oop_part_1/code/src/Item.ts
+++ 02_oop_part_1/code/src/Item.ts
@@ -3,6 +3,9 @@ import { Comparable } from './Comparable';
 let id = 0;
 
 export abstract class Item implements Comparable<Item> {
+    weight: number;
+    value: string;
+    
     public compareTo(other: Item): number {
         // your code goes here
     }
